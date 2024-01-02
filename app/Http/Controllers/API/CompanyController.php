c0c04950cdd7f04348e56b2cf3ef0871683f1cba
Tri Putra Satriawan <tripsatriawan30@gmail.com>
2024-01-02 16:50:58 +0700

    Company API

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -4,26 +4,37 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
 use App\Models\Company;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
-    public function all(Request $request)
+    public function fetch(Request $request)
     {
         $id = $request->input('id');
         $name = $request->input('name');
         $limit = $request->input('limit', 10);
 
+
+        $companiesQuery = Company::with(['users'])->whereHas('users', function ($query) {
+            $query->where('user_id', Auth::id());
+        });
         if ($id) {
-            $company = Company::with(['users'])->find($id);
+            $company = $companiesQuery->find($id);
 
             if ($company) {
                 return ResponseFormatter::success($company, 'Company Found');
             }
             return ResponseFormatter::error('Company Not Found', 404);
         }
-        $companies = Company::with(['users']);
+
+        //mencari data di model company di function users yang user_id nya sama dengan auth id pada yang login
+        $companies = $companiesQuery;
 
         if ($name) {
             $companies->where('name', 'like', '%' . $name . '%');
@@ -33,5 +44,63 @@ class CompanyController extends Controller
             $companies->paginate($limit),
             'Companies found'
         );
+    }
+
+    public function create(CreateCompanyRequest $request)
+    {
+        try {
+
+            //upload logo = jika logo ada maka disimpan di public/logos
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+            //membuat data company
+            $company = Company::create([
+                'name' => $request->name,
+                'logo' => $path
+            ]);
+
+            //jika company tidak ada maka return error
+            if (!$company) {
+                throw new Exception('Company not created');
+            }
+
+            //menambahkan company ke user ke table company_user
+            $user = User::find(Auth::id());
+            $user->companies()->attach($company->id);
+
+            //untuk menambahkan users ke return responseformatter
+            $company->load('users');
+            return ResponseFormatter::success($company, 'Company Created');
+        } catch (Exception $e) {
+            //kalau try error atau tidak ada maka masuk ke sini
+            return ResponseFormatter::error('Company Not Created', 404);
+        }
+    }
+    public function update(UpdateCompanyRequest $request, $id)
+    {
+        try {
+
+            $company = Company::find($id);
+
+            //jika company tidak ada maka return error
+            if (!$company) {
+                throw new Exception('Company not found');
+            }
+            //upload logo = jika logo ada maka disimpan di public/logos
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+            //update data company
+            $company->update([
+                'name' => $request->name,
+                'logo' => $path
+            ]);
+
+            return ResponseFormatter::success($company, 'Company Updated');
+        } catch (Exception $e) {
+            //kalau try error atau tidak ada maka masuk ke sini
+            return ResponseFormatter::error('Company Not Updated', 404);
+        }
     }
 }
